b3b38b096a4192cf878eedf2d3d18afbcce47174
barsheel <barsheel@gmail.com>
2025-03-26 16:31:15 +0700

    add function genDiff() and secondary functions in gendiff.php
    genDiff.php:
    
    add functions:
    - genDiff()
    - arrayCastValuesToString()
    - arrayToString()
    
    delete function:
    - printArray()

--- src/gendiff.php
+++ src/gendiff.php
@@ -8,6 +8,45 @@ namespace Php\Project48\Gendiff;
 
 use Exception;
 use stdClass;
+/**
+ * Compare two flat-json files and return difference
+ *
+ * @param string $pathToFile1
+ * @param string $pathToFile2
+ * @return string
+ */
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $fileArray1 = arrayCastValuesToString(parseJsonFile(buildPath($pathToFile1)));
+    $fileArray2 =  arrayCastValuesToString(parseJsonFile(buildPath($pathToFile2)));
+
+    $resultArray = array_reduce(
+        array_keys($fileArray1),
+        function ($accArray, $key) use ($fileArray1, $fileArray2) {
+            if (!array_key_exists($key, $fileArray2)) {
+                $accArray[] = "- {$key}:{$fileArray1[$key]}";
+            } elseif ($fileArray1[$key] !== $fileArray2[$key]) {
+                $accArray[] = "- {$key}:{$fileArray1[$key]}";
+                $accArray[] = "+ {$key}:{$fileArray2[$key]}";
+            } elseif ($fileArray1[$key] === $fileArray2[$key]) {
+                $accArray[] = "  {$key}:{$fileArray1[$key]}";
+            }
+            return $accArray;
+        },
+        []
+    );
+
+    $fileArray2UniqueElements = array_diff_key($fileArray2, $fileArray1);
+    if ($fileArray2UniqueElements !== []) {
+        $fileArray2UniqueElementsText = array_map(
+            fn ($key) => "+ {$key}:{$fileArray2[$key]}",
+            array_keys($fileArray2UniqueElements)
+        );
+        $resultArray = array_merge($resultArray, $fileArray2UniqueElementsText);
+    }
+
+    return implode("\n", ["\n", ...$resultArray, "\n"]);
+}
 
 /**
  * Convert filename in absolute path
@@ -49,6 +88,7 @@ function parseJsonFile(string $path): array
     return get_object_vars_recursive($data);
 }
 
+
 /**
  * Convert stdClass objects to array
  *
@@ -65,32 +105,66 @@ function get_object_vars_recursive(stdClass $data): array
 }
 
 /**
- * Print array recursively
+ * Cast all values in array to string
  *
- * @param  array   $arrayToPrint
- * @param  integer $offset       - needs to construct indent
- * @return void
+ * @param array $inputArray
+ * @return array
  */
-function printArray(array $arrayToPrint, int $offset = 0): void
+function arrayCastValuesToString(array $inputArray): array
+{
+    return array_map(
+        function($elem) {
+            if (is_array($elem)) {
+                return arrayCastValuesToString($elem);
+            }
+            elseif (is_bool($elem)) {
+                return $elem ? "true" : "false";
+            }
+            else {
+                return strval($elem);
+            }
+        },
+        $inputArray
+    );
+}
+
+
+/**
+ * Output array like string
+ *
+ * @param  array   $inputArray
+ * @param  integer $offset       - needs to construct indent
+ * @return string
+ */
+function arrayToString(array $inputArray, int $offset = 0): string
 {
     $PRINT_ARRAY_BASE_OFFSET = 2;
+
+    $result = [];
 
     $braceOffset = str_repeat(" ", $offset);
     $elementOffset = str_repeat(" ", $offset + $PRINT_ARRAY_BASE_OFFSET);
 
-    print_r("{$braceOffset}{\n");
+    $result = array_reduce(
+        array_keys($inputArray),
+        function ($acc, $key) use ($inputArray, $offset, $elementOffset, $PRINT_ARRAY_BASE_OFFSET) {
+            if (is_array($inputArray[$key])) {
+                $acc[] = "{$elementOffset}{$key}:";
+                $acc[] = arrayToString($inputArray[$key], $offset + $PRINT_ARRAY_BASE_OFFSET * 2);
+            }
+            else {
+               $acc[] = "{$elementOffset}{$key}: {$inputArray[$key]}";
+            }
+            return $acc;
+        },
+        []
+    );
 
-    foreach ($arrayToPrint as $key => $value) {
-        if (is_array($value)) {
-            print_r("{$elementOffset}{$key}");
-            print_r(":\n");
-            printArray($value, $offset + $PRINT_ARRAY_BASE_OFFSET * 2);
-            break;
-        } elseif (is_bool($value)) {
-            $value = $value ? "true" : "false";
-        }
-        print_r("{$elementOffset}{$key}: {$value}\n");
-    }
-
-    print_r("{$braceOffset}}\n");
+    return implode(
+        "\n", 
+        ["{$braceOffset}{",
+        ...$result,
+        "{$braceOffset}}"]);
 }
+
+
