c1b952e89a9c5278c68aabb61702359d29eee56a
barsheel <barsheel@gmail.com>
2025-04-02 01:18:33 +0700

    Update gendiffTest.php
    - add tests for gendiff with YML format
    - refactor all tests

--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -8,11 +8,39 @@ use function Php\Project48\Gendiff\genDiff;
 
 final class GendiffTest extends TestCase
 {
+    private function getFixture(string $filename): string
+    {
+        return (__DIR__ . "/fixtures/" . $filename);
+    }
     public function testFlatJSON(): void
     {
-            $expectedResult = file_get_contents(__DIR__ . "/fixtures/expectedResult1");
-            $result = genDiff("file1.json", "file2.json");
-            file_put_contents("output", $result);
-            $this -> assertEquals($expectedResult, $result);
+        $file1 = $this->getFixture("file1.json");
+        $file2 = $this->getFixture("file2.json");
+
+        $expectedResult = file_get_contents(GendiffTest::getFixture("expected_result1"));
+        $result = genDiff($file1, $file2);
+        file_put_contents("output", $result);
+        $this -> assertEquals($expectedResult, $result);
+
+        $expectedResult = file_get_contents(GendiffTest::getFixture("expected_result2"));
+        $result = genDiff($file2, $file1);
+        file_put_contents("output", $result);
+        $this -> assertEquals($expectedResult, $result);
+    }
+
+    public function testFlatYML(): void
+    {
+        $file1 = $this->getFixture("file1.yml");
+        $file2 = $this->getFixture("file2.yml");
+
+        $expectedResult = file_get_contents(GendiffTest::getFixture("expected_result1"));
+        $result = genDiff($file1, $file2);
+        file_put_contents("output", $result);
+        $this -> assertEquals($expectedResult, $result);
+
+        $expectedResult = file_get_contents(GendiffTest::getFixture("expected_result2"));
+        $result = genDiff($file2, $file1);
+        file_put_contents("output", $result);
+        $this -> assertEquals($expectedResult, $result);
     }
 }
