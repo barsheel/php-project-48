f0b9a233d48f8be11da21ae960793b115977ff75
barsheel <barsheel@gmail.com>
2025-04-09 20:08:26 +0700

    refactor StylishFormatter.php
    refactor stylish formatter

--- src/formatters/stylishFormatter.php
+++ src/formatters/stylishFormatter.php
@@ -2,9 +2,11 @@
 
 namespace Differ\Differ\Formatters\StylishFormatter;
 
+const PRINT_ARRAY_BASE_OFFSET = "  ";
+
 /**
  * convert diff array to stylish-style text
- * @param array $inputArray
+ * @param array $input
  * @return string
  */
 function stylishFormatter(array $inputArray): string
@@ -16,54 +18,62 @@ function stylishFormatter(array $inputArray): string
 /**
  * internal recursive function which forms stylish-style text
  *
- * @param  array   $inputArray
+ * @param  mixed   $input
  * @param  integer $offsetLevel       - needs to construct indent
  * @return string
  */
-function stylishFormatterRecursive(array $inputArray, int $offsetLevel = 0): string
+function stylishFormatterRecursive(mixed $input, int $offsetLevel = 0): string
 {
-    $PRINT_ARRAY_BASE_OFFSET = "  ";
-
-    $parentOffset = str_repeat($PRINT_ARRAY_BASE_OFFSET, $offsetLevel * 2);
-    $elementOffset = "{$parentOffset}{$PRINT_ARRAY_BASE_OFFSET}";
-    $output = [];
-    $prefix = "";
-
-    foreach ($inputArray as $key => $signedElement) {
-        foreach ($signedElement as $sign => $childElement) {
-            switch ($sign) {
-                case "actualValue":
-                    $prefix = "  ";
-                    break;
-                case "oldValue":
-                    $prefix = "- ";
-                    break;
-                case "newValue":
-                    $prefix = "+ ";
-                    break;
-            }
-
-            $value = is_array($childElement)
-            ? stylishFormatterRecursive($childElement, $offsetLevel + 1)
-            : $childElement;
-
-            $output[] = "{$elementOffset}{$prefix}{$key}: {$value}";
-        }
+    //if got value
+    if (!is_array($input)) {
+        return $input;
     }
+
+    //if got indexed array
+    $parentOffset = str_repeat(PRINT_ARRAY_BASE_OFFSET, $offsetLevel * 2);
+    $elementOffset = str_repeat(PRINT_ARRAY_BASE_OFFSET, $offsetLevel * 2 + 1);
+    $result = [];
+
+    $result = array_reduce(
+        $input,
+        function ($acc, $item) use ($elementOffset, $offsetLevel) {
+            $key = $item["key"];
+            switch ($item["type"]) {
+                case "unchanged":
+                    $acc[] = implode([$elementOffset, "  ", $item["key"], ": ", stylishFormatterRecursive($item["value"], $offsetLevel + 1)]);
+                    return $acc;
+                case "added":
+                    $acc[] = implode([$elementOffset, "+ ", $item["key"], ": ", stylishFormatterRecursive($item["new_value"], $offsetLevel + 1)]);
+                    return $acc;
+                case "removed":
+                    $acc[] = implode([$elementOffset, "- ", $item["key"], ": ", stylishFormatterRecursive($item["old_value"], $offsetLevel + 1)]);
+                    return $acc;
+                case "changed":
+                    $acc[] = implode([$elementOffset, "- ", $item["key"], ": ", stylishFormatterRecursive($item["old_value"], $offsetLevel + 1)]);
+                    $acc[] = implode([$elementOffset, "+ ", $item["key"], ": ", stylishFormatterRecursive($item["new_value"], $offsetLevel + 1)]);
+                    return $acc;
+                case "array":
+                    $acc[] = implode([$elementOffset, "  ", $item["key"], ": ", stylishFormatterRecursive($item["children"], $offsetLevel + 1)]);
+                    return $acc;
+            }
+            return $acc;
+        },
+        []
+    );
 
     return implode(
         "\n",
-        ["{", ...$output, "{$parentOffset}}"]
+        ["{", ...$result, "{$parentOffset}}"]
     );
 }
 
 /**
  * сast all values in array to stylish style - it's prepare diff array for output
  *
- * @param array $inputArray
+ * @param array $input
  * @return array
  */
-function arrayCastValuesToString(array $inputArray): array
+function arrayCastValuesToString(array $input): array
 {
     return array_map(
         function ($elem) {
@@ -77,6 +87,6 @@ function arrayCastValuesToString(array $inputArray): array
                 return strval($elem);
             }
         },
-        $inputArray
+        $input
     );
 }
